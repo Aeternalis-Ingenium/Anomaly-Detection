a4cee562544d331f41bdd139d43e9c9c8bbb07a0
N. L. <nino@pleno.earth>
2023-10-18 21:00:32 +0200

    feat(types): create a custom AnomalyDetector type to enable the auto-complete
    Sometimes, having 1 single class as a type, mostly if it's an interface class, will disable the auto-complete and disturb the linters because the child classes will not be visible in the background. In Python 3.12, we have the flexibility to create a simple custom types that lets us still have the accurate type for an object while being able to enjoy the auto-complete and not-so-crazy liter e.g. MyPy.

---
+++ src/anomaly_detection/utils/types.py
@@ -0,0 +1,19 @@
+from anomaly_detection.models.detectors.autoencoder import AutoEncoderAnomalyDetector
+from anomaly_detection.models.detectors.block_maxima import BlockMaximaAnomalyDetector
+from anomaly_detection.models.detectors.dbscan import DBSCANAnomalyDetector
+from anomaly_detection.models.detectors.isolation_forest import IsoForestAnomalyDetector
+from anomaly_detection.models.detectors.mad import MADAnomalyDetector
+from anomaly_detection.models.detectors.one_class_svm import OneClassSVMAnomalyDetector
+from anomaly_detection.models.detectors.pot import POTAnomalyDetector
+from anomaly_detection.models.detectors.zscore import ZScoreAnomalyDetector
+
+AnomalyDetector = (
+    AutoEncoderAnomalyDetector
+    | BlockMaximaAnomalyDetector
+    | DBSCANAnomalyDetector
+    | IsoForestAnomalyDetector
+    | MADAnomalyDetector
+    | OneClassSVMAnomalyDetector
+    | POTAnomalyDetector
+    | ZScoreAnomalyDetector
+)
